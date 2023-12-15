57774c6ae11ffe02f3c8542031a8d888983fcde9
Takahashi-Seiji <takahashiseiji18@gmail.com>
2023-12-14 18:00:23 -0600

    RAILS WATCH-LIST :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'review/new'
-  get 'review/create'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
     resources :bookmarks, only: [:new, :create]
